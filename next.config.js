1acb46303544ca851f467b8b3b776bed7506b277
Jasper Dijt <jasper@jasperdijt.eu>
2023-08-25 16:00:14 +0200

    Setup for static site generation.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/gh-pages-react-app-test",
+  output: "export",
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
